780692ac84b220940a5a71d34ec1c9c47ffdeccd
KaBaJlEP <akopanaa91@gmail.com>
2019-11-12 18:25:32 +0300

    add component Container with some styles attached to it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import { colors } from './styles/global';
+import "./index.scss";
+
+const Container = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  height: auto;
+  transform: translate(-50%, -50%);
+`;
 
 function App() {
-  return (
-    <h1>React-To-Do</h1>
-  );
+  return <div className="wrapper">
+    <Container>
+       
+    </Container>
+  </div>;
 }
 
 export default App;
