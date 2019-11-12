4b91fb2141eddcb2c14554123ec231a108ac6100
KaBaJlEP <akopanaa91@gmail.com>
2019-11-12 09:22:38 +0300

    add global.js

---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+export const colors = {
+    white: "#FFF",
+    black: "#001A34",
+    lightBlack: "#001A3480",
+    blue:  "#001AFF",
+    lightBlue: "#001AFF14",
+    hoverBlue: "#001AFF1F",
+    yellow: "#FECC32", 
+    lightYellow: "#FECC321A"   
+}
