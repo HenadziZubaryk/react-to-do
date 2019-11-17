0cebaf580579d8fba7b391dd19a84cd2a7f3a027
KaBaJlEP <akopanaa91@gmail.com>
2019-11-17 22:54:06 +0300

    update global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,11 +1,11 @@
 export const colors = {
-    white: "#FFF",
+    white: "#FFFFFF",
     gray: "#F3F3F3",
     black: "#001A34",
     lightBlack: "#001A3480",
     blue:  "#001AFF",
     lightBlue: "#001AFF14",
     hoverBlue: "#001AFF1F",
-    yellow: "#FECC32", 
-    lightYellow: "#FECC321A"   
+    yellow: "#FECC32",
+    lightYellow: "#FECC321A"
 }
